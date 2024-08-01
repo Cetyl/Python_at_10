7845ef3ab539a03eecc2119497e82e563e56fcce
Rohan Rama Poojari <123969188+Cetyl@users.noreply.github.com>
2024-08-01 14:30:54 +0200

    Create basic.py #SCRUM-3

---
+++ python-revision/basic.py
@@ -0,0 +1,24 @@
+# hello world string print statement
+print("hello world!")
+
+# Num statement in print function
+print(5)
+
+# Number manipulation in print function
+print(171*241)
+
+# \n function for line spacing
+print("i was made \nfor loving you \nyou were made for loving me")
+
+# separator to add something in between the commas using sep command 
+print("hey jude","na","nanana","nananana","nanana","hey jude" , sep="~")
+print("hey jude","\nna","\nnanana","\nnananana","\nnanana","\nhey jude" , sep="~")
+
+# Reversing the number
+num="1,2,3,4,5,6,7,8,9"
+print(num[::-1])
+
+# Reversing the number ,first convert into string then reverse
+a= 1,2,3,4,5,6,7,8,9
+print (str(a)[:: -1])
+
